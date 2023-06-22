5dedf176ff6521ce263b1b5c1134ba2ed51f7e5a
AYUSH PODDAR <ayushpoddar.098@gmail.com>
2023-06-22 09:13:33 +0530

    Fact_Number Code .

--- src/Factorial_Number.java
+++ src/Factorial_Number.java
@@ -1,9 +1,6 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-
 import java.util.Scanner;
 
-public class Main {
+public class Factorial_Number {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         print("Enter the number : ");
@@ -13,8 +10,8 @@ public class Main {
             fact = fact*i;
         }
         print("The Factorial number of " + number + " is " + fact);
-   }
-   public static void print(String message){
-       System.out.print(message);
-   }
+    }
+    public static void print(String message){
+        System.out.print(message);
+    }
 }
