bb4858e571074cfa1540fbe7b7c8a5944d4fad9f
AYUSH PODDAR <ayushpoddar.098@gmail.com>
2023-07-09 20:15:28 +0530

    Inheritance_code.

---
+++ src/Dog.java
@@ -0,0 +1,51 @@
+public class Dog extends Animal {
+        String earShape;
+        String tailShape;
+        public Dog(){}
+
+    public Dog(String earShape, String tailShape) {
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    public Dog(String type, String size, Double waight, String earShape, String tailShape) {
+        super(type, size, waight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    public String getEarShape() {
+            return earShape;
+        }
+
+        public void setEarShape(String earShape) {
+            this.earShape = earShape;
+        }
+
+        public String getTailShape() {
+            return tailShape;
+        }
+
+        public void setTailShape(String tailShape) {
+            this.tailShape = tailShape;
+        }
+      public void bark(){
+          System.out.println(this.getType()+" is barking");
+      }
+    public void run(){
+        System.out.println(this.getType()+" is running");
+    }
+    public void walk(){
+        System.out.println(this.getType()+" is walking");
+    }
+    public void wagTail(){
+        System.out.println(this .getType()+" has wag tail");
+    }
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                '}';
+    }
+}
