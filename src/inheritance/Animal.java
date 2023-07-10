f6142914e2c6ce772fa199ddf961de0afb6a066d
AYUSH PODDAR <ayushpoddar.098@gmail.com>
2023-07-11 00:08:33 +0530

    Inheritance_code.

---
+++ src/inheritance/Animal.java
@@ -0,0 +1,55 @@
+package inheritance;
+
+public class Animal {
+        private String type;
+        private String size;
+        private Double waight;
+        public Animal(){}
+
+        public Animal(String type, String size, Double waight) {
+            this.type = type;
+            this.size = size;
+            this.waight = waight;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getSize() {
+            return size;
+        }
+
+        public void setSize(String size) {
+            this.size = size;
+        }
+
+        public Double getWaight() {
+            return waight;
+        }
+
+        public void setWaight(Double waight) {
+            this.waight = waight;
+        }
+        public void move(String speed){
+            System.out.println("this "+this.type+"  move "+speed);
+        }
+        public void makesNoise(){
+            System.out.println("this "+this.type+" makes noise");
+        }
+
+        @Override
+        public String toString() {
+            return "Animal{" +
+                    "type='" + type + '\'' +
+                    ", size='" + size + '\'' +
+                    ", waight=" + waight +
+                    '}';
+        }
+    }
+
+
